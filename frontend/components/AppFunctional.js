5631043d804022300d80e5b972b1c5d09014f5b7
Melody Forest <mforest@gsu.edu>
2024-08-18 15:41:58 -0400

    Added custom hook to contain state logic

--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -1,33 +1,85 @@
-import React from "react";
+import React, { useReducer } from "react";
 
 export default function AppFunctional(props) {
+  const { appState, dispatch, onSubmit } = useAppState();
+
   return (
     <div id="wrapper" className={props.className}>
       <div className="info">
-        <h3 id="coordinates">Coordinates (2, 2)</h3>
-        <h3 id="steps">You moved 0 times</h3>
+        <h3 id="coordinates">
+          Coordinates ({appState.positionX}, {appState.positionY})
+        </h3>
+        <h3 id="steps">You moved {appState.steps} times</h3>
       </div>
       <div id="grid">
         {[0, 1, 2, 3, 4, 5, 6, 7, 8].map((idx) => (
-          <div key={idx} className={`square${idx === 4 ? " active" : ""}`}>
-            {idx === 4 ? "B" : null}
+          <div
+            key={idx}
+            className={`square${idx === appState.position ? " active" : ""}`}
+          >
+            {idx === appState.position ? "B" : null}
           </div>
         ))}
       </div>
       <div className="info">
-        <h3 id="message"></h3>
+        <h3 id="message">{appState.message ?? ""}</h3>
       </div>
       <div id="keypad">
-        <button id="left">LEFT</button>
-        <button id="up">UP</button>
-        <button id="right">RIGHT</button>
-        <button id="down">DOWN</button>
-        <button id="reset">reset</button>
+        <button id="left" onClick={() => dispatch({ type: "move_left" })}>
+          LEFT
+        </button>
+        <button id="up" onClick={() => dispatch({ type: "move_up" })}>
+          UP
+        </button>
+        <button id="right" onClick={() => dispatch({ type: "move_right" })}>
+          RIGHT
+        </button>
+        <button id="down" onClick={() => dispatch({ type: "move_down" })}>
+          DOWN
+        </button>
+        <button id="reset" onClick={() => dispatch({ type: "reset" })}>
+          reset
+        </button>
       </div>
-      <form>
+      <form onSubmit={onSubmit}>
         <input id="email" type="email" placeholder="type email"></input>
         <input id="submit" type="submit"></input>
       </form>
     </div>
   );
+}
+
+function useAppState() {
+  const initAppState = {
+    position: 4,
+    positionX: 2,
+    positionY: 2,
+    steps: 0,
+    message: "",
+  };
+
+  const [appState, dispatch] = useReducer(
+    (currentAppState, action) => {
+      let newAppState = null;
+
+      switch (action.type) {
+      }
+
+      return {
+        ...currentAppState,
+        ...(newAppState ?? {}),
+      };
+    },
+    { ...initAppState }
+  );
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return {
+    appState,
+    dispatch,
+    onSubmit,
+  };
 }
